e609c3a168d81e9b5f03596e3ecaa35709501860
Selin ATMACA <selinatmaca@Selins-MacBook-Air.local>
2024-06-09 05:53:07 +0300

    update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const checkbox = document.querySelector("#check");
 //   alert("Butona Tıklandı");
 // }
 
-//iputları izleme yol-1
+//inputları izleme yol-1
 veriInput.addEventListener("change", isimYaz);
 
 function isimYaz(event) {
@@ -45,3 +45,60 @@ checkbox.addEventListener("change", (e) => {
 });
 
 //onclick, onmouseenter, onmouseout, onload, onblur, onfocus,onchange
+
+//! LOCAL STORAGE >
+//BİLMEMİZ GEREKEN 2 FARKLI METHOD VAR
+//Örneğin localstorage içerisine veri eklemek istiyorsak
+
+//! Local Storage >> veriler biz silene kadar durur
+//! Session Storage >> veriler sekme kapatınca silini
+
+//setItem(anahtar,değer). >> veriyi depolama alanına koyar
+
+localStorage.setItem("deneme", "denemelik veri");
+
+//veri çekeceksem >>>yalnızca anahtar >> veriyi alıp getirir
+const localVeri = localStorage.getItem("deneme");
+console.log(localVeri);
+
+//bu da demek oluyor ki istediğim veriyi setıtem ile ekleyebiliyorum
+//ve istediğim zaman da getıtem metodu ile çekebiliyorum
+
+//removeItem() denemelik verinin anhatar değerini silmek olacak
+localStorage.removeItem("deneme");
+
+//her girdiğinde ismini vermek istiyorsak localstorage kaydedriz.
+
+//projeye dön
+//15.satır inputun içerisine
+id = "name-input";
+
+//javada
+const nameInput = document.querySelector("#name-input");
+
+// 3- kullnıcın girdiği ismi tarayıca depolamasında saklama
+nameInput.addEventListener("change", (e) => {
+  console.log(e.target.value);
+  //şimdi localstorage ekle
+  localStorage.setItem("name", e.target.value);
+});
+
+//4- tarayıcadan ismi alma
+const username = localStorage.getItem("name") || "";
+//ekelemk için
+nameInput.value = username;
+
+//javasc. hemde java frameworklarınde çokça kullanılan bir kelime var this anlamına gelen
+//!THIS
+//bit objenin içerisinde kendi değerlerine erişmek için kullanılır
+const car = {
+  marka: "BMW",
+  model: "M5",
+  year: 2016,
+  start: function () {
+    return this.model + " " + this.marka + " çalıştı...";
+    return `${this.marka}, ${this.model} çalıştı...`;
+  },
+};
+
+console.log(car.start());
